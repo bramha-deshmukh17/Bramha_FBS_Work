ed1a430de1b8fc1093b0a7d02a8e73ae57b0d6f1
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-08-19 08:51:59 +0530

    C classwork

---
+++ C/Classwork/logical.c
@@ -0,0 +1,14 @@
+void main(){
+
+	int a=10, b=20;
+	printf("%d\n", a&&b);
+	printf("%d\n", a||b);
+
+	int c=printf("Firstbit")||printf("Solutions");
+	printf("%d\n",c);
+
+	c=printf("Firstbit")&&printf("Solutions");
+	printf("%d",c);
+}
+
+
