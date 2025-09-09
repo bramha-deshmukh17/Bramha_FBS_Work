faa50c1aa493dc94656510445eaba76ac0133747
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-09-09 14:59:08 +0530

    C Assignment 6 corrections

--- C/Assignments/Assignment 6/question_2.c
+++ C/Assignments/Assignment 6/question_2.c
@@ -60,16 +60,27 @@ void prime(){
 }
 
 void armstrong(){
-    int n, r, sum = 0, copy;
+    int n, r, sum = 0, copy, power = 0, res = 1;
 
     printf("Enter a number: ");
     scanf("%d", &n);
     copy = n;
 
+    int dummy = n;
+    while (dummy != 0)
+    {
+        dummy /= 10;
+        ++power;
+    }
     while (n > 0)
     {
         r = n % 10;
-        sum += (r * r * r);
+        res = 1;
+
+        for (int i = 1; i <= power; i++)
+            res *= r;
+
+        sum += (res);
 
         n /= 10;
     }
