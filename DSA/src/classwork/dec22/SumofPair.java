86147b51018c2e63c6a3b734a92009de2b3e7148
bramha-deshmukh17 <bramhadeshmukh3@gmail.com>
2025-12-24 10:19:43 +0530

    DSA classwork

--- DSA/src/classwork/dec22/SumofPair.java
+++ DSA/src/classwork/dec22/SumofPair.java
@@ -6,22 +6,22 @@ public class SumofPair {
 
 	public static void main(String[] args) {
 
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print("Enter array size: ");
-//		int size = sc.nextInt();
-//		int[] arr = new int[size];
-//
-//		System.out.print("Enter array elements: ");
-//		for (int i = 0; i < size; i++) {
-//			arr[i] = sc.nextInt();
-//		}
-//
-//		System.out.println("Enter num");
-//		int num = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter array size: ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
 
-		int size = 6, num = 6;
+		System.out.print("Enter array elements: ");
+		for (int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
 
-		int[] arr = { 1, 4, 3,  2, 10, -4 };
+		System.out.println("Enter num");
+		int num = sc.nextInt();
+//
+//		int size = 6, num = 6;
+//
+//		int[] arr = { 1, 4, 3,  2, 10, -4 };
 
 		for (int i = 0; i < size; i++) {
 			for (int j = i + 1; j < size; j++) {
@@ -32,7 +32,7 @@ public class SumofPair {
 			}
 		}
 
-//		sc.close();
+		sc.close();
 
 	}
 
